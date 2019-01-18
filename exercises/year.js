065d36c3c2d3f701b931bd599c1bce31b0d56d58
oraclerevolution <mac.n.drix@gmail.com>
2019-01-18 20:11:53 +0000

    ajout de la fonction year

---
+++ exercises/year.js
@@ -0,0 +1,30 @@
+'use strict'
+/*
+* creer une fonction fromYear qui pendra un seul paramètre ( un nombre entier ) une année et la
+* fonction fromYear doit retourner le siecle de l'année entrée en paramètre.
+*
+* Vous devez avant tout maitriser les notions de fonctions, et d'opérations
+*/
+
+
+//Commencez à écrire votre code ici
+
+
+
+
+
+/*
+* ces lignes d'en bas vont tester votre code et vous dire si cest juste ou pas donc n'y touchez pas
+*/
+
+//* Début des tests du code de l'étudiant
+
+const verif = reuire('assert')
+
+verif.strictEqual(typeof fromYear, 'function')
+verif.strictEqual(fromYear.length, 1)
+verif.strictEqual(fromYear.toString().includes('Math.imul'), false)
+verif.strictEqual(fromYear(1905), 20)
+verif.strictEqual(fromYear(1700), 17)
+
+// Fin des tests tests */
